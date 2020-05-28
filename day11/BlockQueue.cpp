fe6de0f156f64d7b6423c147b33436a0485cc296
GeniusPanYiHao <1542029272@qq.com>
2020-05-28 14:51:54 +0800

    BlockQueue

--- day11/BlockQueue.cpp
+++ day11/BlockQueue.cpp
@@ -2,6 +2,8 @@
 #include<pthread.h>
 #include<iostream>
 #include<queue>
+
+#define THREADCOUNT 4
 class BlockQueue 
 {
   public:
@@ -21,14 +23,40 @@ class BlockQueue
     }
     int pop(int* date)
     {
+      pthread_mutex_lock(&QueueMutex);
+      while(Queue.empty())
+      {
+        pthread_cond_wait(&ConsumeCond,&QueueMutex);
+      }
      *date=Queue.front();
       Queue.pop();
+      pthread_mutex_unlock(&QueueMutex); 
+      pthread_cond_signal(&ProduceCond);
+      pthread_cond_signal(&ConsumeCond);
     }
     int push(int &date)
     {
+      pthread_mutex_lock(&QueueMutex);
+      while(isFull())
+      {
+        pthread_cond_wait(&ProduceCond,&QueueMutex);
+      }
       Queue.push(date);
+      pthread_mutex_unlock(&QueueMutex);
+      pthread_cond_signal(&ConsumeCond);
+      pthread_cond_signal(&ProduceCond);
     }
 
+  private:
+    bool isFull()
+    {
+      if(Queue.size()==Capacity)
+      {
+        return true;
+      }
+      else
+        return false;
+    }
   private:
     std::queue<int> Queue;
     size_t Capacity;
@@ -37,6 +65,57 @@ class BlockQueue
 
     pthread_cond_t ConsumeCond;
     pthread_cond_t ProduceCond;
+    void* Consume_Start(void* arg)
+    {
+      BlockQueue* bq=(BlockQueue*)arg;
+      while(1)
+      {
+        int data;
+        bq->pop(&data);
+        printf("Consume_Start[%p][%d]\n",pthread_self(),data);
+      }
 
+      return NULL;
+    }
+    void* Produce_Start(void* arg)
+    {
+      BlockQueue* bq=(BlockQueue*)arg;
+      int i=0;
+      while(1)
+      {
+          int data;
+          bq->push(i);
+          printf("Produce_Start[%p][%d]\n",pthread_self(),i);
+
+          i++;
+      }
+      return NULL;
+    }
 
 };
+
+int main()
+{
+    pthread_t con_tid[THREADCOUNT],pro_tid[THREADCOUNT];
+      BlockQueue* bq=new BlockQueue(5);
+
+    int i=0;
+    for(;i<THREADCOUNT;i++)
+    {
+      int ret=pthread_create(&con_tid[i],NULL,Consume_Start,(void*)bq);
+      if(ret!=0)
+      {
+        printf("create thread failed\n");
+        return 0;
+      }
+      ret=pthread_create(&pro_tid[i],NULL,Produce_Start,(void*)bq); 
+      if(ret!=0)
+      {
+        printf("create thread failed\n");
+      return 0;
+      }
+    }
+    delete bq;
+    bq=NULL;
+    return 0;
+}
